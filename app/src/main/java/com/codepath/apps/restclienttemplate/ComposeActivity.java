de6c8200306552e5975fb96ae9f5527f1f01d0ac
Grace Cao <gracecao@fb.com>
2018-07-03 15:58:16 -0700

    Added a counter for user to see how many characters are remaining for a tweet out of 140.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,8 +3,11 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -16,18 +19,25 @@ import cz.msebera.android.httpclient.Header;
 
 public class ComposeActivity extends AppCompatActivity {
 
+    private final int MAX_NUM_CHAR = 140;
+
     TwitterClient client;
     EditText userMessage;
     String message;
     Tweet tweet;
+    TextView charCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         client = TwitterApplication.getRestClient(this);
         setContentView(R.layout.activity_compose);
+
         userMessage = (EditText) findViewById(R.id.etCompose);
+        charCount = (TextView) findViewById(R.id.tvCharCount);
+        userMessage.addTextChangedListener(textEditorWatcher);
     }
+
 
     public void onSubmit(View v) {
         message = userMessage.getText().toString();
@@ -53,4 +63,21 @@ public class ComposeActivity extends AppCompatActivity {
         });
 
 }
+private final TextWatcher textEditorWatcher = new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        int numCharactersLeft = MAX_NUM_CHAR - s.length();
+    charCount.setText(String.valueOf(numCharactersLeft));
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+};
 }
